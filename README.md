0631a6b6e428e652205e78f9c1950e55d0f98785
Tomasz Rakowski <tomasz.rakowski@indexexchange.com>
2019-06-18 15:25:47 -0400

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
+[![Build Status](https://travis-ci.org/akayami/console-level.svg?branch=master)](https://travis-ci.org/akayami/console-level)[![Coverage Status](https://coveralls.io/repos/github/akayami/console-level/badge.svg?branch=master)](https://coveralls.io/github/akayami/console-level?branch=master)
 # console-level
 A filter allowing setting of log level for console object
+
+## Usage
+```javascript
+const { Console } = require('console');
+console = new Console({ stdout: process.stdout, stderr: process.stderr });
+// Make console output only warns and bellow
+console = level(console, 'warn');
+console.warn('Some Warning'); 			// stderr will contain this
+console.log('Suppressed level'); 		// Will not appear
+console.level('log');					// Switches log level to log
+console.log('Not suppressed anymore'); 	// Will appear in stdout
+console.level('warn');					// Switches log level back to warn
+```
+
+### Levels
+```javascript
+const stack = {
+	'debug': 20, // stdout
+	'info': 30, // stdout
+	'log': 30, // stdout
+	'warn': 40, // stderr
+	'error': 50, // stderr
+	'trace': 60 // stderr
+};
+```
+
+### Another usage example
+
+```javascript
+const { Console } = require('console');
+console = new Console({ stdout: process.stdout, stderr: process.stderr });
+// Make console output only warns and bellow
+console = level(console, (process.env.dev ? 'debug' : 'warn'));
+
+```
---
+++ test/bbd.test.js
@@ -0,0 +1,99 @@
+const { Console } = require('console');
+const { expect } = require('chai');
+
+const level = require('../lib/main');
+const StreamTest = require('streamtest');
+const MemoryStream = require('memorystream');
+
+describe('BBD Tests', () => {
+
+	it('Needs to allow for standard setup', done => {
+		let out = new MemoryStream(null, {readable: false});
+		let err = new MemoryStream(null, {readable: false});
+		console = new Console({ stdout: out, stderr: err });
+		console = level(console);
+		console.log('test');
+		expect(out.toString()).to.equal(`test\n`);
+		expect(err.toString()).to.equal('');
+		done();
+	});
+	
+	it('Needs to filter out levels', done => {
+		let out = new MemoryStream(null, {readable: false});
+		let err = new MemoryStream(null, {readable: false});
+		console = new Console({ stdout: out, stderr: err });
+		console = level(console, 'error');
+		console.log('test');
+		expect(out.toString()).to.equal('');
+		expect(err.toString()).to.equal('');
+		done();
+	});
+	
+	let levels = {
+		'debug': 'out',
+		'info': 'out',
+		'log': 'out',
+		'warn': 'err',
+		'error': 'err',
+		//'trace': 'err',
+	};
+
+	Object.keys(levels).forEach((l) => {
+//		process.stdout.write(l);
+		it(`Testing level ${l}`, done => {
+		 	let streams = {};
+
+			streams['err'] = new MemoryStream(null, {readable: false});
+			streams['out'] = new MemoryStream(null, {readable: false});
+
+			console = new Console({ stdout: streams['out'], stderr: streams['err'] });
+			console = level(console, l);
+			console[l]('test');
+			expect(streams[levels[l]].toString()).to.equal(`test\n`);
+		 	done();
+		});
+	});
+	
+	let tracelvl = {
+		// 'debug': 'out',
+		// 'info': 'out',
+		// 'log': 'out',
+		// 'warn': 'err',
+		// 'error': 'err',
+		'trace': 'err',
+	};
+
+	Object.keys(tracelvl).forEach((l) => {
+//		process.stdout.write(l);
+		it(`Testing level ${l}`, done => {
+			let streams = {};
+
+			streams['err'] = new MemoryStream(null, {readable: false});
+			streams['out'] = new MemoryStream(null, {readable: false});
+
+			console = new Console({ stdout: streams['out'], stderr: streams['err'] });
+			console = level(console, l);
+			console[l]('test-trace');
+			expect(streams[tracelvl[l]].toString()).to.contain(`test-trace\n`);
+			done();
+		});
+	});
+
+	
+	it('Needs to support runtime level changing', done => {
+		let out = new MemoryStream(null, {readable: false});
+		let err = new MemoryStream(null, {readable: false});
+		console = new Console({ stdout: out, stderr: err });
+		console = level(console, 'log');
+		console.log('test');
+		expect(out.toString()).to.equal(`test\n`);
+		expect(err.toString()).to.equal('');
+		console.log('test2');
+		expect(out.toString()).to.equal(`test\ntest2\n`);
+		console.level('error');
+		console.log('lala');
+		expect(out.toString()).to.equal(`test\ntest2\n`);
+		done();
+	})
+	
+});
